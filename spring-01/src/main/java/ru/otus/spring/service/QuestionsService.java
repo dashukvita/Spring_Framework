be421803ce4070c7452ca12b5a9c5e87da9a0ebc
4studywork <4studywork@gmail.com>
2020-06-15 20:27:48 +0300

    delete files

--- spring-01/src/main/java/ru/otus/spring/service/QuestionsService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-
-public interface QuestionsService {
-    Map<String, ArrayList<String>> getQuestions() throws IOException;
-}
--- spring-01/src/main/java/ru/otus/spring/service/ReadWriteService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.service;
-
-public interface ReadWriteService {
-    void write(String line);
-    void writeResult(String format, Object... args);
-    String read();
-}
